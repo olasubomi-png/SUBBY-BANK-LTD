129cfd5cab930baf8f929ccdf9d2a18fbf0080a6
olasubomi-png <vegasola8@gmail.com>
2026-08-24 17:48:35 +0100

    Initialize Flutter app with routing and provider

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/login_screen.dart';
+import 'screens/dashboard_screen.dart';
+import 'providers/auth_provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => AuthProvider(),
+      child: MaterialApp(
+        title: 'SUBBY Bank',
+        theme: ThemeData.dark(),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => LoginScreen(),
+          '/dashboard': (context) => DashboardScreen(),
+        },
+      ),
+    );
+  }
+}
